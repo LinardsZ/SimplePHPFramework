2afa66b11e9ebfb69f2d594d5d3cd502c64b07f1
Linards Zilitis <l.zilitis@gmail.com>
2023-02-24 18:29:13 +0200

    create default controller

---
+++ app/controllers/Pages.php
@@ -0,0 +1,15 @@
+<?php
+
+class Pages {
+  public function __construct() {
+    echo "test";  
+  }
+
+  public function index() {
+    
+  }
+
+  public function homepage(...$params) {
+    
+  }
+}
