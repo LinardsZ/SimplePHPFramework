943069d24ecb2df5476c739de6d05afb34990bde
Linards Zilitis <l.zilitis@gmail.com>
2023-02-26 15:54:38 +0200

    update database values for testing

--- app/config/config.php
+++ app/config/config.php
@@ -4,6 +4,6 @@ define("URLROOT", "http://testinglinards.com");
 define("SITENAME", "SimplePHPFramework");
 
 define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "P@ssword!");
+define("DB_USER", "linards");
+define("DB_PASS", "P@ssword1");
 define("DB_NAME", "SimplePHPFramework");
